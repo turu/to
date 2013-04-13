891356b230cfb6943e391fa0898d847156879deb
Piotr Turek <piotr.turu.turek@gmail.com>
2013-04-13 13:04:10 +0200

    TO1 init

---
+++ lab02/dice-game/src/main/java/pl/edu/agh/to1/dice/App.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.to1.dice;
+
+import pl.edu.agh.to1.dice.logic.DiceGame;
+
+public class App {
+    public static void main(String[] args) {
+        new DiceGame().play();
+    }
+}
---
+++ lab02/dice-game/src/main/java/pl/edu/agh/to1/dice/logic/DiceGame.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.to1.dice.logic;
+
+public class DiceGame {
+
+    public void play() {
+        System.out.println("Playing Dice");
+        // TODO
+    }
+}
